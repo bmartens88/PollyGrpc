26c05af67a75e35e3b845935677caae89f57258b
Bas Martens <bmartens1988@kpnmail.nl>
2022-02-08 13:24:31 +0100

    Example code

---
+++ GrpcServer/Services/Service.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services;
+
+public class Service : Greeter.GreeterBase
+{
+    private readonly ILogger<Service> _logger;
+
+    public Service(ILogger<Service> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        var rnd = new Random();
+        var res = rnd.NextDouble() * 10;
+        return res > 5
+            ? Task.FromResult(new HelloReply
+            {
+                Message = $"Hello {request.Name}"
+            })
+            : throw new RpcException(new Status(StatusCode.Internal, "Internal server error"), new Metadata());
+    }
+}
---
+++ PollyGrpc/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using Grpc.Core;
+using GrpcServer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+var services = new ServiceCollection();
+var loggerFactory = LoggerFactory.Create(logging =>
+{
+    logging.AddConsole();
+    logging.SetMinimumLevel(LogLevel.Debug);
+});
+
+var serverErrors = new HttpStatusCode[]
+{
+    HttpStatusCode.BadGateway,
+    HttpStatusCode.GatewayTimeout,
+    HttpStatusCode.ServiceUnavailable,
+    HttpStatusCode.InternalServerError,
+    HttpStatusCode.TooManyRequests,
+    HttpStatusCode.RequestTimeout
+};
+
+var grpcErrors = new StatusCode[]
+{
+    StatusCode.DeadlineExceeded,
+    StatusCode.Internal,
+    StatusCode.NotFound,
+    StatusCode.ResourceExhausted,
+    StatusCode.Unavailable,
+    StatusCode.Unknown
+};
+
+Func<HttpRequestMessage, IAsyncPolicy<HttpResponseMessage>> retryFunc = (request) =>
+{
+    return Policy.HandleResult<HttpResponseMessage>(r =>
+        {
+            var grpcStatus = StatusManager.GetStatusCode(r);
+            var httpStatusCode = r.StatusCode;
+
+            return grpcStatus == null && serverErrors.Contains(httpStatusCode) ||
+                   httpStatusCode == HttpStatusCode.OK && grpcErrors.Contains(grpcStatus.Value);
+        })
+        .WaitAndRetryAsync(3, (input) => TimeSpan.FromSeconds(3 + input), (result, timeSpan, retryCount, context) =>
+        {
+            var grpcStatus = StatusManager.GetStatusCode(result.Result);
+            Console.WriteLine($"Request failed with {grpcStatus}. Retry");
+        });
+};
+
+services.AddGrpcClient<Greeter.GreeterClient>(o => { o.Address = new Uri("https://localhost:7234"); })
+    .AddPolicyHandler(retryFunc);
+
+var provider = services.BuildServiceProvider();
+var client = provider.GetRequiredService<Greeter.GreeterClient>();
+
+try
+{
+    var reply = await client.SayHelloAsync(new HelloRequest {Name = "Bas"});
+    Console.WriteLine($"Received: {reply.Message}");
+}
+catch (RpcException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+internal static class StatusManager
+{
+    public static StatusCode? GetStatusCode(HttpResponseMessage response)
+    {
+        var headers = response.Headers;
+
+        if (!headers.Contains("grpc-status") && response.StatusCode == HttpStatusCode.OK)
+            return StatusCode.OK;
+
+        if (headers.Contains("grpc-status"))
+            return (StatusCode) int.Parse(headers.GetValues("grpc-status").First());
+
+        return null;
+    }
+}
